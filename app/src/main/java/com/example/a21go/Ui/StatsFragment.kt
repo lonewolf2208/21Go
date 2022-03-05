a2f9e17f9a79784cadadf6fcab4213a8f0f12d5b
Jyotsna <guptajyotsna2002@gmail.com>
2022-03-06 00:43:13 +0530

    Awards done

--- app/src/main/java/com/example/a21go/Ui/StatsFragment.kt
+++ app/src/main/java/com/example/a21go/Ui/StatsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.a21go.Ui
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -14,6 +15,8 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.a21go.Network.Response
 import com.example.a21go.R
+import com.example.a21go.Ui.Splash_Screen.Companion.data
+import com.example.a21go.Ui.Splash_Screen.Companion.loggedIn
 import com.example.a21go.Ui.Splash_Screen.Companion.readInfo
 import com.example.a21go.ViewModel.StatsViewModel
 import com.example.a21go.databinding.StatsFragmentBinding
@@ -35,6 +38,8 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
     lateinit var items:List<StatsModel>
     var userid:String?=null
     var Date:Int?=null
+    var attemp:Int?=null
+    var best:Int?=null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -78,11 +83,78 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
 
 
 
-       // customCalendar.monthYearTextView.setTextColor(Color.parseColor("#ffffff"))
+        customCalendar.monthYearTextView.setTextColor(Color.parseColor("#ffffff"))
         customCalendar.setOnNavigationButtonClickedListener(CustomCalendar.PREVIOUS, this)
         customCalendar.setOnNavigationButtonClickedListener(CustomCalendar.NEXT, this)
 
+        data.observe(viewLifecycleOwner, {
+            when (it) {
+                is Response.Success -> {
+                   attemp= it.data?.attempts?.toInt()
+                    best=it.data?.best
 
+                    if(loggedIn==true){
+
+                        binding.imageView9.setImageResource(R.drawable.ic_pic4)
+                    }
+                    when(best){
+                        1-> binding.imageView10.setImageResource(R.drawable.ic_group__11_)
+                        2->binding.imageView11.setImageResource(R.drawable.ic_group_25)
+                        4->binding.imageView12.setImageResource(R.drawable.ic_group_27)
+                        7->binding.imageView13.setImageResource(R.drawable.ic_group_29)
+                        14->binding.imageView15.setImageResource(R.drawable.ic_group_35)
+                        21->binding.imageView16.setImageResource(R.drawable.ic_group__12_)
+                    }
+                    when(attemp)
+                    {
+                        1->binding.imageView17.setImageResource(R.drawable.ic_pic5)
+                        10->binding.imageView18.setImageResource(R.drawable.ic_pic3)
+                        50->binding.imageView14.setImageResource(R.drawable.ic_group_32)
+                    }
+                }
+                is Response.Error -> {
+                    Toast.makeText(
+                        context,
+                        it.errorMessage,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+
+            }
+        })
+        binding.imageView9.setOnClickListener {
+            Toast.makeText(requireContext(), "Congratulations on starting your journey", Toast.LENGTH_SHORT).show()
+
+        }
+
+        binding.imageView10.setOnClickListener {
+            Toast.makeText(requireContext(), "Reach Day 1", Toast.LENGTH_SHORT).show()
+        }
+        binding.imageView11.setOnClickListener {
+            Toast.makeText(requireContext(), "Reach Day 2", Toast.LENGTH_SHORT).show()
+        }
+        binding.imageView12.setOnClickListener {
+            Toast.makeText(requireContext(), "Reach Day 4", Toast.LENGTH_SHORT).show()
+        }
+        binding.imageView13.setOnClickListener {
+            Toast.makeText(requireContext(), "Reach Day 7", Toast.LENGTH_SHORT).show()
+        }
+        binding.imageView15.setOnClickListener {
+            Toast.makeText(requireContext(), "Reach Day 14", Toast.LENGTH_SHORT).show()
+        }
+        binding.imageView16.setOnClickListener {
+            Toast.makeText(requireContext(), "Reach Day 21", Toast.LENGTH_SHORT).show()
+        }
+        binding.imageView17.setOnClickListener {
+            Toast.makeText(requireContext(), "Complete your journey in 1 go", Toast.LENGTH_SHORT).show()
+        }
+        binding.imageView18.setOnClickListener {
+            Toast.makeText(requireContext(), "Complete your journey in 10 go's", Toast.LENGTH_SHORT).show()
+        }
+        binding.imageView14.setOnClickListener {
+            Toast.makeText(requireContext(), "Complete your journey in 50 go's", Toast.LENGTH_SHORT).show()
+        }
 
 
         return view
@@ -99,7 +171,7 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
             Calendar.JANUARY -> {
                 arr[0] = HashMap()
                 if(Calendar.JANUARY<Calendar.MONTH){
-                    for (i in 1..customCalendar.allViews.size)
+                    for (i in 1..31)
                     {
                         arr[0]?.set(i, "present")
                     }
@@ -161,11 +233,11 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
                         arr[0]?.set(k.toInt(), "absent")
                     }
                 }
-             }
+            }
             Calendar.MARCH -> {
                 arr[0] = HashMap()
                 if(Calendar.MARCH<Calendar.MONTH){
-                    for (i in 1..customCalendar.allViews.size)
+                    for (i in 1..31)
                     {
                         arr[0]?.set(i, "present")
                     }
@@ -198,7 +270,7 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
             Calendar.APRIL -> {
                 arr[0] = HashMap()
                 if(Calendar.APRIL<Calendar.MONTH){
-                    for (i in 1..customCalendar.allViews.size)
+                    for (i in 1..30)
                     {
                         arr[0]?.set(i, "present")
                     }
@@ -231,7 +303,7 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
             Calendar.MAY -> {
                 arr[0] = HashMap()
                 if(Calendar.MAY<Calendar.MONTH){
-                    for (i in 1..customCalendar.allViews.size)
+                    for (i in 1..31)
                     {
                         arr[0]?.set(i, "present")
                     }
@@ -260,11 +332,11 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
                         arr[0]?.set(k.toInt(), "absent")
                     }
                 }
-                    }
+            }
             Calendar.JUNE -> {
                 arr[0] = HashMap()
                 if(Calendar.JUNE<Calendar.MONTH){
-                    for (i in 1..customCalendar.allViews.size)
+                    for (i in 1..30)
                     {
                         arr[0]?.set(i, "present")
                     }
@@ -297,7 +369,7 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
             Calendar.JULY -> {
                 arr[0] = HashMap()
                 if(Calendar.JULY<Calendar.MONTH){
-                    for (i in 1..customCalendar.allViews.size)
+                    for (i in 1..31)
                     {
                         arr[0]?.set(i, "present")
                     }
@@ -330,7 +402,7 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
             Calendar.AUGUST -> {
                 arr[0] = HashMap()
                 if(Calendar.AUGUST<Calendar.MONTH){
-                    for (i in 1..customCalendar.allViews.size)
+                    for (i in 1..31)
                     {
                         arr[0]?.set(i, "present")
                     }
@@ -363,7 +435,7 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
             Calendar.SEPTEMBER -> {
                 arr[0] = HashMap()
                 if(Calendar.SEPTEMBER<Calendar.MONTH){
-                    for (i in 1..customCalendar.allViews.size)
+                    for (i in 1..30)
                     {
                         arr[0]?.set(i, "present")
                     }
@@ -392,11 +464,11 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
                         arr[0]?.set(k.toInt(), "absent")
                     }
                 }
-                 }
+            }
             Calendar.OCTOBER -> {
                 arr[0] = HashMap()
                 if(Calendar.OCTOBER<Calendar.MONTH){
-                    for (i in 1..customCalendar.allViews.size)
+                    for (i in 1..31)
                     {
                         arr[0]?.set(i, "present")
                     }
@@ -429,7 +501,7 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
             Calendar.NOVEMBER -> {
                 arr[0] = HashMap()
                 if(Calendar.NOVEMBER<Calendar.MONTH){
-                    for (i in 1..customCalendar.allViews.size)
+                    for (i in 1..30)
                     {
                         arr[0]?.set(i, "present")
                     }
@@ -462,7 +534,7 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
             Calendar.DECEMBER -> {
                 arr[0] = HashMap()
                 if(Calendar.DECEMBER<Calendar.MONTH){
-                    for (i in 1..customCalendar.allViews.size)
+                    for (i in 1..31)
                     {
                         arr[0]?.set(i, "present")
                     }
@@ -492,6 +564,8 @@ class StatsFragment :Fragment(), OnNavigationButtonClickedListener {
                     }
                 }
             }
+
+
 
 
         }
